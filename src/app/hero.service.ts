aee03eb4b9d84cf70cff0721f8bb47a0c4cd049a
Sylwester Biega <biegson11@gmail.com>
2022-01-27 22:40:23 +0100

    add HttpClient module. add InMemoryModule, service. implement httpclient into hero service - getHeroes. add errorHandling to hero service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { from, Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs';
+
 import { MessageService } from './message.service';
 
 import { Hero } from './hero';
@@ -10,19 +13,49 @@ import { HEROES } from './mock-heroes';
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes';
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+    ) { }
+
+  log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  /*
+ * Handle Http operation that failed.
+ * Let the app continue.
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
+  private handleError<T>(operation = 'operation', result?: T){
+    return (error: any): Observable<T> => {
+      // TODO: send the eror to remote loggin infrastructure
+      console.error(error);
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T)
+    }
+  }
+  
 
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl).pipe(
+      tap(_ => this.log('fetched heroes')),
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+    );
   }
 
   getHero(id: number): Observable<Hero> {
     // For now, assume that a hero with the specified `id` always exists.
     // Error handling will be added in the next step of the tutorial.
     const hero = HEROES.find(h => h.id === id)!;
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    this.log(`fetched hero id=${id}`);
     return of(hero);
   }
 
